d8976c73adc7886f90d390808a6e1eb432e254f4
josema03 <jose.marin1997@gmail.com>
2021-03-28 20:50:45 -0400

    feat: mikro-orm and typegraphql crud

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from '../entities/Post';
 import { MyContext } from 'src/types';
-import { Query, Resolver, Ctx, Arg, Int } from 'type-graphql';
+import { Query, Resolver, Ctx, Arg, Int, Mutation } from 'type-graphql';
 
 @Resolver()
 export class PostResolver {
@@ -14,5 +14,38 @@ export class PostResolver {
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
     return em.findOne(Post, { _id });
+  }
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title', () => String) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+  @Mutation(() => Post)
+  async updatePost(
+    @Arg('_id', () => Int) _id: number,
+    @Arg('title', () => String) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { _id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== 'undefined') {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
+  }
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg('_id', () => Int) _id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { _id });
+    return true;
   }
 }
